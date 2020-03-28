b3afd3c89489b7e2f77788b7692acf58de2e0577
Valentino Simamora <43459761+nnadnad@users.noreply.github.com>
2020-03-29 02:59:59 +0700

    fixing respawn

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -9,7 +9,10 @@ public class GameMaster : MonoBehaviour {
     public Transform playerPrefabs;
     public Transform spawnPlayerPoint;
 
-    public int spawnDelay = 2;
+    //for effect respawn
+    public Transform spawnPrefabs;
+
+    public float spawnDelay = 2;
 
     private void Start() {
         if (gameMaster == null) {
@@ -18,9 +21,16 @@ public class GameMaster : MonoBehaviour {
     }
 
     public IEnumerator RespawnPlayer(){
-        Debug.Log("AUDIO GOES HERE");
+        GetComponent<AudioSource>().Play();
+        // Debug.Log("AUDIO GOES HERE");
         yield return new WaitForSeconds(spawnDelay);
         Instantiate(playerPrefabs, spawnPlayerPoint.position, spawnPlayerPoint.rotation);
+        Transform spawn = (Transform) Instantiate(spawnPrefabs, spawnPlayerPoint.position, spawnPlayerPoint.rotation);
+
+        spawn.parent = spawnPlayerPoint;
+
+        Destroy(spawn.gameObject, 2f);
+        // Debug.Log("Spawn Particles for effect");
     }
 
     public static void KillPlayer (Player  player) {
