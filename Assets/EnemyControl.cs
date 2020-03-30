903201760728ec0832c9138b4a0e7b68430c98be
Valentino Simamora <43459761+nnadnad@users.noreply.github.com>
2020-03-31 02:51:21 +0700

    add enemy GUI

--- Assets/EnemyControl.cs
+++ Assets/EnemyControl.cs
@@ -6,16 +6,49 @@ public class EnemyControl : MonoBehaviour
 {
 	[System.Serializable]
 	public class EnemyStats {
-		public int Health = 100;
+		// public int Health = 100;
+        public int maxHealth = 100;
+        private int _currentHealth;
+        public int currentHealth {
+            get { 
+                return _currentHealth; 
+            }
+            set { 
+                _currentHealth = Mathf.Clamp(value, 0, maxHealth); 
+            }
+        }
+
+        public void Init() {
+            currentHealth = maxHealth;
+        }
+
 	}
 	
 	public EnemyStats stats = new EnemyStats();
+
+    [SerializeField]
+    private StatusBar statusBar;
+
+    private void Start() {
+        
+        stats.Init();
+        if (statusBar != null) {
+            statusBar.SetHealth(stats.currentHealth, stats.maxHealth);
+        }
+    }
 	
 	public void DamageEnemy (int damage) {
-		stats.Health -= damage;
-		if (stats.Health <= 0)
+		stats.currentHealth -= damage;
+		if (stats.currentHealth <= 0)
 		{
 			GameMaster.KillEnemy (this);
 		}
+
+        if (statusBar != null) {
+            statusBar.SetHealth(stats.currentHealth, stats.maxHealth);
+        }
+
+
+
 	}
 }
---
+++ Assets/StatusBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform HealthBar;
+
+    [SerializeField]
+    private Text HealthStatus;
+
+
+
+    private void Start() {
+        if (HealthBar ==  null) {
+            Debug.LogError("No health bar references");
+        }
+        if (HealthStatus ==  null) {
+            Debug.LogError("No health text references");
+        }
+    }
+
+    public void SetHealth(int _current, int _max) {
+        float _value = (float) _current  / _max;
+        HealthBar.localScale = new Vector3(_value, HealthBar.localScale.y, HealthBar.localScale.z);
+        HealthStatus.text = _current + "/" + _max + " HP";
+    }
+
+
+
+
+}
