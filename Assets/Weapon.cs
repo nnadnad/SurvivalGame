d4e716dc3fb0a9c3b2272d74256fa95deea62cc6
Valentino Simamora <43459761+nnadnad@users.noreply.github.com>
2020-03-31 00:41:58 +0700

    add shoot animasi

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -15,6 +15,8 @@ public class Weapon : MonoBehaviour
     public float effectSpawnRate = 10;
     private float timeToSpawn = 0;
 
+    public Transform HitPrefabs;
+
     private float castFire = 0;
     Transform firePoint;
 
@@ -48,11 +50,11 @@ public class Weapon : MonoBehaviour
 
         RaycastHit2D hit = Physics2D.Raycast (firePointPos, (mousePosition-firePointPos), 100, whatToHit);
 
-        if (Time.time >= timeToSpawn) {
-            Effect();
-            // StartCoroutine("Effect");
-            timeToSpawn = Time.time + 1/effectSpawnRate;
-        }
+        // if (Time.time >= timeToSpawn) {
+        //     Effect();
+        //     // StartCoroutine("Effect");
+        //     timeToSpawn = Time.time + 1/effectSpawnRate;
+        // }
 
         // Effect();
 
@@ -66,11 +68,52 @@ public class Weapon : MonoBehaviour
                 Debug.Log("We hit " + hit.collider.name + " and did " + damage + " damage.");
             }
         }
+
+        if (Time.time >= timeToSpawn) {
+
+            Vector3 hitPos;
+            Vector3 hitNormal;
+
+            if (hit.collider == null) {
+                hitPos = (mousePosition - firePointPos)*50;
+                hitNormal = new Vector3(9999,9999,9999);
+            } else {
+                hitPos = hit.point;
+                hitNormal = hit.normal;
+            }
+
+
+            Effect(hitPos, hitNormal);
+            // StartCoroutine("Effect");
+            timeToSpawn = Time.time + 1/effectSpawnRate;
+        }
+
+
+
+        
     }
 
 
-    void Effect() {
-        Instantiate (FireBulletPrefab, firePoint.position, firePoint.rotation);
+    void Effect(Vector3 hitPos, Vector3 hitNormal) {
+        Transform trail = (Transform) Instantiate (FireBulletPrefab, firePoint.position, firePoint.rotation);
+
+        LineRenderer lr = trail.GetComponent<LineRenderer>();
+
+        if (lr != null) {
+            // SET POSITIONS
+
+            lr.SetPosition (0, firePoint.position);
+            lr.SetPosition(1, hitPos);
+        }
+        
+        Destroy(trail.gameObject, 0.05f);
+
+        if (hitNormal != new Vector3 (9999, 9999, 9999)) {
+            Transform hitParticle = (Transform) Instantiate(HitPrefabs, hitPos, Quaternion.FromToRotation(Vector3.right, hitNormal));
+            Destroy(hitParticle.gameObject, 0.5f);
+        }
+
+
         Transform flash = (Transform) Instantiate (FlashPrefab, firePoint.position, firePoint.rotation);
 
         flash.parent = firePoint;
